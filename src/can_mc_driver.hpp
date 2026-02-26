ad5db02849cd286bad08f0b6b7e5b8c2165d9881
Patryk Dudizński <dodpat02@gmail.com>
2026-02-27 00:17:29 +0100

    up

--- src/can_mc_driver.hpp
+++ src/can_mc_driver.hpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <cstring>
 #include <functional>
+#include <iostream>
 #include <memory>
 
 #include "mcan_base_module_dummy.hpp"
@@ -16,6 +17,8 @@ namespace mcan {
 
 using namespace mcan_basic_module;
 using namespace mcan_base_module_dummy;
+
+#define LOG_MSG(msg) std::cout << "[CAN MC Driver] " << msg << std::endl;
 
 template<typename McCanSlaveInterface, typename Hardware>
 class McSlaveDriver
@@ -40,23 +43,80 @@ class McSlaveDriver
 
   Status start_driver()
   {
-    ARI_RETURN_ON_ERROR(generic_switch_mode(DeviceMode::CONFIGURATION));
+    switch_to_state(DeviceMode::CONFIGURATION);
     return Status::OK();
   }
 
   Status control_loop()
   {
-    switch (_mode) {
-      case DeviceMode::NORMAL:
-        return mode_normal_loop();
-        break;
-      case DeviceMode::CONFIGURATION:
-        return mode_configuration_loop();
-        break;
-      default:
-        break;
+    if (execute_state() && _func_main_loop) {
+      return _func_main_loop();
     }
     return Status::OK();
+  }
+
+  void switch_to_state(DeviceMode next_state)
+  {
+    if (_state == next_state) {
+      return;
+    }
+    _next_state = next_state;
+    _switch_state = true;
+  }
+
+  bool execute_state()
+  {
+    if (!_switch_state) {
+      return true;
+    }
+
+    // Exit current state
+    if (_func_exit_prev_state) {
+      auto exit_status = _func_exit_prev_state();
+      if (!exit_status.ok()) {
+        LOG_MSG("Failed to exit state: " << exit_status.to_string());
+        return false;
+      }
+    }
+
+    DeviceMode next_state = _next_state;
+    _switch_state = false;
+
+    // Configure functions for next state
+    switch (next_state) {
+      case DeviceMode::NORMAL:
+        _func_main_loop = std::bind(&McSlaveDriver::mode_normal_loop, this);
+        _func_exit_prev_state = std::bind(&McSlaveDriver::mode_exit_normal, this);
+        _func_enter_next_state = std::bind(&McSlaveDriver::mode_enter_normal, this);
+        break;
+      case DeviceMode::CONFIGURATION:
+        _func_main_loop = std::bind(&McSlaveDriver::mode_configuration_loop, this);
+        _func_exit_prev_state = std::bind(&McSlaveDriver::exit_configuration_mode, this);
+        _func_enter_next_state =
+          std::bind(&McSlaveDriver::enter_configuration_mode, this);
+        break;
+      default:
+        LOG_MSG("Unknown state");
+        return false;
+    }
+
+    // Update state
+    _state = next_state;
+
+    // Enter new state
+    if (_func_enter_next_state) {
+      auto enter_status = _func_enter_next_state();
+      if (!enter_status.ok()) {
+        LOG_MSG("Failed to enter state: " << enter_status.to_string());
+        return false;
+      }
+      // Check if state changed during enter
+      if (next_state != _next_state) {
+        _func_exit_prev_state = nullptr;
+      }
+    }
+
+    return next_state == _next_state;
   }
 
   McCanSlaveInterface& get_interface() { return _interface; }
@@ -66,42 +126,6 @@ class McSlaveDriver
     : _can_interface(std::move(can_interface))
     , _uid_21_bit(uid_21_bit)
   {
-  }
-
-  Status generic_switch_mode(DeviceMode new_mode)
-  {
-    if (new_mode == _mode) {
-      return Status::OK(); // Already in the desired mode
-    }
-
-    // Exit current mode
-    if (_mode_exit_mode_func) {
-      ARI_RETURN_ON_ERROR(_mode_exit_mode_func());
-    }
-
-    switch (new_mode) {
-      case DeviceMode::NORMAL:
-        _mode_exit_mode_func = std::bind(&McSlaveDriver::mode_exit_normal, this);
-        _mode_enter_mode_func = std::bind(&McSlaveDriver::mode_enter_normal, this);
-        break;
-      case DeviceMode::CONFIGURATION:
-        _mode_exit_mode_func = std::bind(&McSlaveDriver::exit_configuration_mode, this);
-        _mode_enter_mode_func = std::bind(&McSlaveDriver::enter_configuration_mode, this);
-        break;
-      default:
-        // Handle unknown mode error
-        break;
-    }
-
-    // Update mode
-    _mode = new_mode;
-
-    // Enter new mode
-    if (_mode_enter_mode_func) {
-      ARI_RETURN_ON_ERROR(_mode_enter_mode_func());
-    }
-
-    return Status::OK();
   }
 
   Status mode_normal_loop()
@@ -202,12 +226,15 @@ class McSlaveDriver
     ARI_RETURN_ON_ERROR(_can_interface->remove_callback(mcan_connect_msg_id_with_node_id(
       configs::GetHardwareType::k_base_address, _node_id, true)));
     // ARI_RETURN_ON_ERROR(_can_interface->remove_callback(
-    // connect_msg_id_with_node_id(configs::SetDeviceNodeId::k_base_address,
-    // _node_id)));
+    //   connect_msg_id_with_node_id(configs::SetDeviceNodeId::k_base_address,
+    //   _node_id)));
     ARI_RETURN_ON_ERROR(_can_interface->remove_callback(mcan_connect_msg_id_with_node_id(
       configs::EnterConfigurationMode::k_base_address, 1, true)));
     ARI_RETURN_ON_ERROR(_can_interface->remove_callback(mcan_connect_msg_id_with_node_id(
       configs::PingModule::k_base_address, _node_id, true)));
+
+    ARI_RETURN_ON_ERROR(_can_interface->remove_callback(mcan_connect_msg_id_with_node_id(
+      configs::FlashIndicatorLed::k_base_address, _node_id)));
 
     std::apply(
       [&](auto&&... args) {
@@ -247,13 +274,11 @@ class McSlaveDriver
     cd.send(response_frame);
   }
 
-  // void callback_nm_set_device_node_id(CanBase &cd, const CanFrame &frame,
-  // void *args);
   void callback_nm_enter_configuration_mode(CanBase& cd,
                                             const CanFrame& frame,
                                             void* args)
   {
-    (void)(generic_switch_mode(DeviceMode::CONFIGURATION));
+    switch_to_state(DeviceMode::CONFIGURATION);
   }
 
   void callback_ping_module(CanBase& cd, const CanFrame& frame, void* args)
@@ -347,21 +372,24 @@ class McSlaveDriver
       return; // error unpacking
     }
     _node_id = msg_buffer.value;
-    (void)generic_switch_mode(DeviceMode::NORMAL);
+    switch_to_state(DeviceMode::NORMAL);
   }
 
   void callback_enter_configuration_mode(CanBase& cd, const CanFrame& frame, void* args)
   {
-    (void)(generic_switch_mode(DeviceMode::CONFIGURATION));
+    switch_to_state(DeviceMode::CONFIGURATION);
   }
 
   std::shared_ptr<CanBase> _can_interface;
   Hardware _hardware;
   uint32_t _uid_21_bit;
 
-  DeviceMode _mode{ DeviceMode::UNDEFINED };
-  std::function<Status()> _mode_exit_mode_func = nullptr;
-  std::function<Status()> _mode_enter_mode_func = nullptr;
+  DeviceMode _state{ DeviceMode::UNDEFINED };
+  DeviceMode _next_state{ DeviceMode::UNDEFINED };
+  bool _switch_state{ false };
+  std::function<Status()> _func_main_loop = nullptr;
+  std::function<Status()> _func_exit_prev_state = nullptr;
+  std::function<Status()> _func_enter_next_state = nullptr;
 
   McCanSlaveInterface _interface;
 
